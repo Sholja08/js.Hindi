34bff87a059f61c676e5acfc7f9a852d2aa064d6
Sholja Chauhan <150106372+Sholja08@users.noreply.github.com>
2025-07-06 11:00:55 +0000

    variables

--- 01_basics/operator.js
+++ 01_basics/operator.js
@@ -1,3 +1,13 @@
-let score = 66
-console.log(typeof score);
-console.log(typeof (score));
+// let score = 66
+// console.log(typeof score);
+// console.log(typeof (score));
+// let isLoggedIn = "sholja"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+
+
